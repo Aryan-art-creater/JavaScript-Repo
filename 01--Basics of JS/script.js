5e20b5fa7a643b808fc32fa8c35864dc37ee546c
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-03 19:32:51 +0530

    Update script.js

--- 01--Basics of JS/script.js
+++ 01--Basics of JS/script.js
@@ -1,24 +1,26 @@
+"use strict";
+
 alert("Hello World");
 
-console.log("Code is running...")
-console.log("Code is also running...")
-console.log("Code is looking like wow...")
+console.log("Code is running...");
+console.log("Code is also running...");
+console.log("Code is looking like wow...");
 
 
 // var is function-scoped, let is block-scoped (avoid var in modern JS)
-let a = Number(prompt("Enter your number"))
-let userConfirmed = confirm("Are you sure you want to leave this page and blast your computer ")
+let a = Number(prompt("Enter your number"));
+let userConfirmed = confirm("Are you sure you want to leave this page and blast your computer ");
 
 if(userConfirmed){
-    console.log("computer is blasting ")
+    console.log("computer is blasting ");
 }
 
 else{
-    console.log("computer is not blasting ")
+    console.log("computer is not blasting ");
 }
 
-console.log("your number is " + a) // Here value of a is change on the basis of if and else.
+console.log("your number is " + a); // Here value of a is change on the basis of if and else.
 
-document.title = "Hey gun park" // For title
+document.title = "Hey gun park"; // For title
 
-document.body.style.backgroundColor = "red" // For background color 
+document.body.style.backgroundColor = "red"; // For background color 
