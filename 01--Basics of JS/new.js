86781825ccd16d0483578e783bee566df1c6360a
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-03 19:31:55 +0530

    Update new.js

--- 01--Basics of JS/new.js
+++ 01--Basics of JS/new.js
@@ -1,5 +1,7 @@
-console.log("Hello World") // String type 
-console.log("106") // number type
-console.log("Code is running...") 
-console.log("Code is also running...")
-console.log("Code is looking like wow...")
+"use strict";
+
+console.log("Hello World"); // String type 
+console.log("106"); // number type
+console.log("Code is running..."); 
+console.log("Code is also running...");
+console.log("Code is looking like wow...");
