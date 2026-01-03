892f37b52987611cd9f886e47311103c8349c0c9
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-03 16:00:54 +0530

    Update script.js

--- Basics of JS/script.js
+++ Basics of JS/script.js
@@ -4,6 +4,8 @@ console.log("Code is running...")
 console.log("Code is also running...")
 console.log("Code is looking like wow...")
 
+
+// var is also like using Let 
 var a = prompt("Enter your number")
 var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
 
@@ -15,8 +17,8 @@ else{
     console.log("computer is not blasting ")
 }
 
-console.log("your number is " + a)
+console.log("your number is " + a) // Here value of a is change on the basis of if and else.
 
-document.title = "Hey gun park"
+document.title = "Hey gun park" // For title
 
-document.body.style.backgroundColor = "red"
+document.body.style.backgroundColor = "red" // For background color 
