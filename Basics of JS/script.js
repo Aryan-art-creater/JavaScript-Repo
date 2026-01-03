91eeb0d803cf43c3c0a9dbef40d9805181c5aef5
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-03 15:41:00 +0530

    Basics of JS

---
+++ Basics of JS/script.js
@@ -0,0 +1,22 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+
+if(isTrue){
+    console.log("computer is blasting ")
+}
+
+else{
+    console.log("computer is not blasting ")
+}
+
+console.log("your number is " + a)
+
+document.title = "Hey gun park"
+
+document.body.style.backgroundColor = "red"
