f98925d3ef591ec8c2021c52b7c440bc615f0aa6
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-03 15:42:31 +0530

    Variables, Data Types & objects of JS

---
+++ Variables, Data Types & Objects of JS/script.js
@@ -0,0 +1,45 @@
+console.log("Hey this is tutorial 55")
+
+let a = 5;
+// a = a + 1
+let b = 6;
+let c = "aryan";
+let _a = "aditya";
+// var 55a = "jatin"; //  Not Allowed
+
+console.log( a + b + 7 )
+console.log( a + b + 7 + c)
+console.log( a + b + 7 , c , _a)
+
+console.log(typeof a , typeof b , typeof c)
+
+{
+    let a = 90;
+    console.log(a)
+}
+
+// const a1 = 10
+// a1 = a1 + 1
+// Not Allowed because a1 is const.
+
+let p = "Aryan";
+let q = 2;
+let r = 3.4556;
+const s = true;
+let x = undefined;
+let y = null; 
+
+console.log(p ,q ,r ,s ,x ,y)
+console.log(typeof p ,typeof q ,typeof r ,typeof s ,typeof x ,typeof y)
+
+let o = {
+    "name": "Aryan",
+    "Job Code": 5000,
+    "is_handsome": true
+}
+
+console.log(o)
+o.salary = "100 Cr";
+console.log(o)
+o.salary = "500 Cr";
+console.log(o)
