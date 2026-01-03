50ef7a99ccec3ca7ea8ca1eabd7140b546b26ba1
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-03 19:37:06 +0530

    Update script.js

--- 03--Conditions - if, else if, else ladder of JS/script.js
+++ 03--Conditions - if, else if, else ladder of JS/script.js
@@ -1,17 +1,17 @@
 "use strict";
 
-console.log("Hello I am conditional tutorial")
+console.log("Hello I am conditional tutorial");
 
 let age = 21;
 // let grace = 2;
 
 // age += grace // it means add grace into age and that's our new age.
-// console.log(age)
-// console.log(age - grace) // subtract
-// console.log(age * grace) // multiply
-// console.log(age / grace) // divide
-// console.log(age ** grace) // exponentiation
-// console.log(age % grace) // modulus
+// console.log(age);
+// console.log(age - grace); // subtract
+// console.log(age * grace); // multiply
+// console.log(age / grace); // divide
+// console.log(age ** grace); // exponentiation
+// console.log(age % grace); // modulus
 
 
 // && -> both conditions must be true
