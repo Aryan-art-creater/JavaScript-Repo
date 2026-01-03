1a6b914bf2a17ba92a939069cf9588c745ddb9c2
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-03 19:04:04 +0530

    Loops

---
+++ 04--JS Loops/index.js
@@ -0,0 +1,57 @@
+console.log("Hey I am a tutorial on loop")
+
+// let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+
+// for loop !
+
+// for (let i = 0; i < 40; i++) {
+//     console.log(a+i);
+// }
+
+let obj ={
+    name: "Aryan",
+    work: "Coding",
+    compan: "MC"
+}
+
+// for in loop !
+
+// for (const key in obj) {
+//         console.log(key);
+// }
+
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key, element);
+// }
+
+
+// for of loop !
+
+// for (const v of "Aryan") {
+//     console.log(v)
+// }
+
+
+// while loop !
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(i)
+//     i++;
+// }
+
+
+// do-while loop !
+
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++;
+// } while (i < 6);
