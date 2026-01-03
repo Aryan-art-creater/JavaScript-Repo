8e6b0e69a8a638d749baa875f531fa6a1ee81827
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-03 15:55:18 +0530

    Update new.js

--- Basics of JS/new.js
+++ Basics of JS/new.js
@@ -1,4 +1,5 @@
-console.log("Hello World") 
+console.log("Hello World") // String type 
+console.log("106") // number type
 console.log("Code is running...") 
 console.log("Code is also running...")
 console.log("Code is looking like wow...")
