cd8f545769d2a656a956e0e324e1cb736fff7ca0
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-05 09:50:32 +0530

    Update index.js

--- 06--JS Strings/index.js
+++ 06--JS Strings/index.js
@@ -2,14 +2,14 @@
 
 console.log("JavaScript Strings");
 
-let a = "Aryan";
+const a = "Aryan";
 console.log(a);
 console.log(a[0]);
 console.log(a[1]);
 console.log(a[2]);
 console.log(a[3]);
 console.log(a[4]);
-console.log(a[5]);
+console.log(a[5]); // undefined
 console.log(a.length);
 
 let real_name = "Aryan";
@@ -18,12 +18,12 @@ let friend = "Aditya";
 console.log("The MC of this anime is " + real_name + " and MF is " + friend );
 console.log(`The MC of this anime is ${real_name} and MF is ${friend}`);
 
-let b = "Naruto"
+const b = "Naruto"
 
 console.log(b.toUpperCase());
 console.log(b.toLowerCase());
 console.log(b.length);
 console.log(b.slice(1, 5));
 console.log(b.slice(1));
-console.log(b.replace("arut" , "****"));
-console.log(b.concat(a, " Hinata" , "Sanjana"));
+console.log(b.replace("arut" , "****")); // replace returns a new string, original string remains unchanged
+console.log(`${b} ${a} Hinata Sanjana`);
