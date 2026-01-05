af68977f87f2810a3186b697cb906b9195e70d71
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-05 09:21:55 +0530

    function

--- 05--JS Functions/script.js
+++ 05--JS Functions/script.js
@@ -1,39 +1,28 @@
 "use strict";
 
 function nice(name) {
-    console.log("Hey " + name + " you are nice!");
-    console.log("Hey " + name + " you are good!");
-    console.log("Hey " + name + " you look sexy!");
-    console.log("Hey " + name + " you are very rich!");
+  console.log(`Hey ${name}, you look confident!`);
+  console.log(`Hey ${name}, you are doing great!`);
 }
 
 nice("Aditya");
 
-
 function sum(a, b, c = 5) {
-    // a and b are non const and c is const.
-    // console.log(a + b);
-    console.log(a, b, c);
-    return a + b + c;
+  return a + b + c;
 }
 
-result1 = sum(2, 3, 2); // sum of a, b, c is result1
-result2 = sum(5, 3);  // if value of c is not given then c is 5.
-result3 = sum(2, 20);
-result4 = sum(25, 3);
-result5 = sum(2, 36);
+const result1 = sum(2, 3, 2);
+const result2 = sum(5, 3);
+const result3 = sum(2, 20);
 
-console.log("The sum of these numbers is : " + result1);
-console.log("The sum of these numbers is : " + result2);
-console.log("The sum of these numbers is : " + result3);
-console.log("The sum of these numbers is : " + result4);
-console.log("The sum of these numbers is : " + result5);
+console.log(`Result 1: ${result1}`);
+console.log(`Result 2: ${result2}`);
+console.log(`Result 3: ${result3}`);
 
+const func1 = (x) => {
+  console.log("I am an arrow function", x);
+};
 
-const func1 = (x)=>{
-    console.log("I am an arrow function", x);
-}
-
-func1(23); // func1(x) put any value of x.
+func1(23);
 func1(99);
 func1(55);
