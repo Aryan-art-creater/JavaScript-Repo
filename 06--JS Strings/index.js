2aed6f728406b0a5086f2162749e4251af7ec005
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-05 09:41:48 +0530

    string

---
+++ 06--JS Strings/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+console.log("JavaScript Strings");
+
+let a = "Aryan";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a.length);
+
+let real_name = "Aryan";
+let friend = "Aditya";
+
+console.log("The MC of this anime is " + real_name + " and MF is " + friend );
+console.log(`The MC of this anime is ${real_name} and MF is ${friend}`);
+
+let b = "Naruto"
+
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1, 5));
+console.log(b.slice(1));
+console.log(b.replace("arut" , "****"));
+console.log(b.concat(a, " Hinata" , "Sanjana"));
