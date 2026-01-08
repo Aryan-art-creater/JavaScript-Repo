5d068483a424b6a09dfc85a8ba3b4d1ca1dedfd5
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-08 13:38:27 +0530

    Update script.js

--- 10--JS - Selecting by Ids, Classes, and More/script.js
+++ 10--JS - Selecting by Ids, Classes, and More/script.js
@@ -7,6 +7,6 @@ boxes[2].style.backgroundColor = "yellow";
 
 document.getElementById("redbox").style.backgroundColor = "skyblue"
 
-document.querySelector(".box").style.backgroundColor = "red";
+document.querySelector(".box").style.backgroundColor = "red"; // it selects only the first child
 
 document.querySelectorAll(".box").forEach(e => e.style.backgroundColor = "blue");
