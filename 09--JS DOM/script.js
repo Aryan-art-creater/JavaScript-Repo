65e3b744cda3d5294f9ac6293f4f84f0e4d7e76f
Aryan-art-creater <aryanjangra556690@gmail.com>
2026-01-08 13:14:00 +0530

    DOM

---
+++ 09--JS DOM/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+console.log(document.body.childNodes);
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+console.log(document.body.firstElementChild);
+console.log(document.body.lastElementChild);
+console.log(document.body.firstElementChild.childNodes);
+console.log(document.body.firstElementChild.children);
+console.log(document.body.firstElementChild.firstChild);
+console.log(document.body.firstElementChild.lastChild);
+console.log(document.body.firstElementChild.firstElementChild);
+console.log(document.body.firstElementChild.lastElementChild);
+console.log(document.body.firstElementChild.parentNode);
+
+console.log(document.body.firstElementChild.children[2].previousElementSibling);
+console.log(document.body.firstElementChild.children[2]);
+console.log(document.body.firstElementChild.children[2].nextElementSibling);
